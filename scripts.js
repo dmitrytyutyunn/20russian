53265f8b9e6dfdf60fa240a03b4903a31c1a6e91
dmitrytyutyunn <60935158+dmitrytyutyunn@users.noreply.github.com>
2020-03-16 14:54:24 +0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,73 @@
+function oncreate(){
+    ///
+}
+
+function checkVocabulary(){
+    document.getElementById("FrameOfLesson").src="https://dmitrytyutyunn.github.io/Guessword/";
+}
+
+function chooseLesson(clicked){
+	
+	switch(clicked){
+		case "button1":
+           document.getElementById("FrameOfLesson").src="hour1.html";
+		   break;
+		case "button2":
+           document.getElementById("FrameOfLesson").src="hour2.html";
+		   break;
+		case "button3":
+           document.getElementById("FrameOfLesson").src="hour3.html";
+		   break;
+		case "button4":
+           document.getElementById("FrameOfLesson").src="hour4.html";
+		   break;
+		case "button5":
+           document.getElementById("FrameOfLesson").src="hour5.html";
+		   break;
+		case "button6":
+           document.getElementById("FrameOfLesson").src="hour6.html";
+		   break;
+		case "button7":
+           document.getElementById("FrameOfLesson").src="hour7.html";
+		   break;
+		case "button8":
+           document.getElementById("FrameOfLesson").src="hour8.html";
+		   break;
+		case "button9":
+           document.getElementById("FrameOfLesson").src="hour9.html";
+		   break;
+		case "button10":
+           document.getElementById("FrameOfLesson").src="hour10.html";
+		   break;
+		case "button11":
+           document.getElementById("FrameOfLesson").src="hour11.html";
+		   break;
+		case "button12":
+           document.getElementById("FrameOfLesson").src="hour12.html";
+		   break;
+		case "button13":
+           document.getElementById("FrameOfLesson").src="hour13.html";
+		   break;
+		case "button14":
+           document.getElementById("FrameOfLesson").src="hour14.html";
+		   break;
+		case "button15":
+           document.getElementById("FrameOfLesson").src="hour15.html";
+		   break;
+		case "button16":
+           document.getElementById("FrameOfLesson").src="hour16.html";
+		   break;
+		case "button17":
+           document.getElementById("FrameOfLesson").src="hour17.html";
+		   break;
+		case "button18":
+           document.getElementById("FrameOfLesson").src="hour18.html";
+		   break;
+		case "button19":
+           document.getElementById("FrameOfLesson").src="hour19.html";
+		   break;
+		case "button20":
+           document.getElementById("FrameOfLesson").src="hour20.html";
+		   break;
+	} 
+}
